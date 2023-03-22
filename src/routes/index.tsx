66a8926990b16b6f1d69d2dffd61f318c9a00739
JusAeng <justo_play@hotmail.com>
2023-03-23 02:21:46 +0700

    routes handle

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Navigate, Route, Routes as RRDRoutes } from "react-router-dom";
+import HomePage from "../pages/home";
+
+const Routes = () => {
+  return (
+    <RRDRoutes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </RRDRoutes>
+  );
+};
+
+export default Routes;
