cc411eef49e38387a3088ee69e48485ab31b4ab4
JusAeng <justo_play@hotmail.com>
2023-03-23 15:33:24 +0700

    separete layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import map_pic from "@/assets/pics/thailand-map.jpg";
 
 const HomePage = () => {
-  return <div>Nani</div>;
+  return (
+    <div className="h-[100vh] bg-slate-300 grid place-items-center">
+      <div className="h-4/5 w-4/5 bg-white grid grid-cols-2">
+        <section>
+          <img src={map_pic} className="w-full h-full object-contain" />
+        </section>
+        <section>2</section>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
