9e51f3d412244a89b81014a2db77a1d628b08e69
JusAeng <justo_play@hotmail.com>
2023-03-23 16:06:32 +0700

    thailand map pic

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import map_pic from "@/assets/pics/thailand-map.jpg";
+import map_pic from "@/assets/pics/thailand-map-poster.png";
 
 const HomePage = () => {
   return (
     <div className="h-[100vh] bg-slate-300 grid place-items-center">
-      <div className="h-4/5 w-4/5 bg-white grid grid-cols-2">
+      <div className="h-[90%] w-[80%] bg-white grid grid-cols-2">
         <section>
-          <img src={map_pic} className="w-full h-full object-contain" />
+          <img src={map_pic} alt="" className="mx-auto" />
         </section>
         <section>2</section>
       </div>
