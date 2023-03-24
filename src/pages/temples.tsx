d27cb473ee09ff27967e95328a1f2492bb3b7f72
JusAeng <justo_play@hotmail.com>
2023-03-25 04:56:59 +0700

    remove temples page

--- src/pages/temples.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TemplesPerform from "@/components/TemplesPerform";
-
-const TemplesPage = () => {
-  return <div className="bg-ocean"></div>;
-};
-
-export default TemplesPage;
