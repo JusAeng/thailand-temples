3de4064402ddec3c2220c2a9c128ca4da4dfeb89
JusAeng <justo_play@hotmail.com>
2023-03-25 02:39:29 +0700

    no

--- src/pages/temples.tsx
+++ src/pages/temples.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import TemplesPerform from "@/components/TemplesPerform";
 
 const TemplesPage = () => {
-  return (
-    <div className="bg-ocean">
-      <TemplesPerform />
-    </div>
-  );
+  return <div className="bg-ocean"></div>;
 };
 
 export default TemplesPage;
